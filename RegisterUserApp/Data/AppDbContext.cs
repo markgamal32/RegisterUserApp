290bb0a8d4d9b255afe5e1be98e53c3a3c9fd3c2
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2025-04-03 16:31:49 +0200

    Add project files.

---
+++ RegisterUserApp/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RegisterUserApp.Models;
+
+namespace RegisterUserApp.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
